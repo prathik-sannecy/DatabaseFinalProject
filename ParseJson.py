f153e4b5c5752c2582e6482a1f01b84e94a69639
prathik <prathik@pdx.edu>
2019-10-25 23:17:53 -0700

    Added function to parse NBA json data, output into a table format.
    Added prelimanary datasets (plus parsed output)

---
+++ ParseJson.py
@@ -0,0 +1,18 @@
+
+def parseNBAJsonFile(inputFileName, outputFileName):
+    with open(inputFileName) as jsonFile, open(outputFileName, "w") as out:
+        text = jsonFile.read()
+        splitText = text.split("{")
+        parameters = splitText[2]
+        table = splitText[3]
+        splitTable = table.split("[")
+        tableHeader = splitTable[1].replace('],"rowSet":', "")
+        out.write(tableHeader + "\n")
+
+        for row in range(5, len(splitTable)):
+            if row != "":
+                out.write(splitTable[row].replace("]", "").strip(",").strip("}") + "\n")
+
+
+parseNBAJsonFile("DataSets/leaguedashplayerstats.json", "Outputs/outplayer.csv")
+parseNBAJsonFile("DataSets/leaguedashteamstats.json", "Outputs/outteam.csv")
